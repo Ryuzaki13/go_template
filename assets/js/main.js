dad695c03e14e98dd8d26cbebf34eb9fc92a1682
Ryuzaki13 <ryuzaki_dk_rm_guitar@mail.ru>
2022-04-05 10:45:33 +0500

    npm added

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,67 @@ function SEND(method, url, data, callback) {
     let xhr = new XMLHttpRequest();
     xhr.open(method, url);
     xhr.onload = (ev) => {
-        callback(JSON.parse(ev.target.response));
+        callback && callback(JSON.parse(ev.target.response));
     };
     xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
     xhr.send(JSON.stringify(data));
+}
+
+let buttonAddTask = document.querySelector("#AddTask");
+if (buttonAddTask) {
+    buttonAddTask.onclick = function () {
+        let inputs = document.querySelectorAll(".form > input");
+        let data = {};
+
+        for (let i = 0; i < inputs.length; i++) {
+            data[inputs[i].name] = inputs[i].value;
+        }
+
+        SEND("PUT", "/task", data, buildTasks);
+    }
+}
+
+function buildTasks(r) {
+    let blocks = {
+        active: document.querySelector(`[data-status="active"]`),
+        complete: document.querySelector(`[data-status="complete"]`),
+        delete: document.querySelector(`[data-status="delete"]`),
+    }
+
+    for (let i = 0; i < r; i++) {
+        let div = document.createElement("div");
+        div.dataset.id = r.id;
+        div.draggable = true;
+        div.className = "item";
+        div.textContent = r.name;
+        blocks[r.status].append(div);
+    }
+}
+
+// DragAndDrop
+
+let dragElements = document.querySelectorAll(".task-list .item");
+for (let i = 0; i < dragElements.length; i++) {
+    dragElements[i].ondragstart = onDrag;
+}
+
+let dropElements = document.querySelectorAll(".task-list > div");
+for (let i = 0; i < dropElements.length; i++) {
+    dropElements[i].ondrop = onDrop;
+    dropElements[i].ondragover = onDragOver;
+}
+
+function onDrag(e) {
+    e.dataTransfer.setData("item", e.target.dataset.id);
+}
+
+function onDrop(e) {
+    e.preventDefault();
+    let itemId = e.dataTransfer.getData("item");
+    let item = document.querySelector('[data-id="' + itemId + '"]');
+    item && e.target.append(item);
+}
+
+function onDragOver(e) {
+    e.preventDefault();
 }
--- main.go
+++ main.go
@@ -4,6 +4,15 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Task struct {
+	ID     int    `json:"id"`
+	Name   string `json:"name"`
+	Text   string `json:"text"`
+	Status string `json:"status"` // active | complete | delete
+}
+
+var tasks []Task
+
 func main() {
 	router := gin.Default()
 
@@ -12,6 +21,9 @@ func main() {
 	router.Static("assets", "assets")
 
 	router.GET("/", index)
+	router.PUT("/task", addTask)
+	router.GET("/task", getTasks)
+	router.POST("/task", updateStatus)
 
 	_ = router.Run("127.0.0.1:8080")
 }
@@ -19,3 +31,45 @@ func main() {
 func index(c *gin.Context) {
 	c.HTML(200, "index", nil)
 }
+
+func getTasks(c *gin.Context) {
+	c.JSON(200, tasks)
+}
+
+func updateStatus(c *gin.Context) {
+	var task Task
+
+	e := c.BindJSON(&task)
+	if e != nil {
+		c.JSON(400, nil)
+		return
+	}
+
+	for i := range tasks {
+		if task.ID == tasks[i].ID {
+			tasks[i].Status = task.Status
+			break
+		}
+	}
+
+	c.JSON(200, tasks)
+}
+
+func addTask(c *gin.Context) {
+	var task Task
+
+	e := c.BindJSON(&task)
+	if e != nil {
+		c.JSON(400, nil)
+		return
+	}
+
+	task.ID = len(tasks) + 1
+	task.Status = "active"
+
+	tasks = append(tasks, task)
+
+	c.JSON(200, tasks)
+}
+
+// https://github.com/Ryuzaki13/go_template
